b9ba438b511ceb217ee8e3e6ba3460227ccea317
lilouNait <38019413+lilouNait@users.noreply.github.com>
2018-04-10 15:10:23 +0200

    Update acheteur.h

--- acheteur.h
+++ acheteur.h
@@ -1,7 +1,16 @@
 #include <iostream>
-
-class Acheteur: public Client{
+#include <string>
+Cass Acheteur: public Client{
 protected:
 
+ int prix;
+ bool proposition;
+ vector <int> Achat;
+
 public:
+    Acheteur::Acheteur();
+    void NouvelleVisite (Biens &bien, int prix, );
+    void ConsultVisite (Biens &bien);
+    bool Proposition (Biens &bien, int prix )
+    ~Acheteur ();
 };
