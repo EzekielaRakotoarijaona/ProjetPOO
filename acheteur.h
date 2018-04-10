7c12bac66c0d5fe6753b46c456aad2ea21ce9aa0
lilouNait <38019413+lilouNait@users.noreply.github.com>
2018-04-10 16:06:30 +0200

    Update acheteur.h

--- acheteur.h
+++ acheteur.h
@@ -5,12 +5,41 @@ protected:
 
  vector <int> prix;
  vector <bool> proposition;
- vector <int> Achat;
+ vector <int> visite;
 
 public:
-    Acheteur::Acheteur(int prix, bool proposition);
-    bool NouvelleVisite (int bien, int prix);
-    tab ConsultVisite ();
-    bool Proposition (int bien, int prix);
+    Acheteur(int prix, bool proposition)
+    {
+     prix ={};
+     proposition= false;
+     viste ={};
+    };
+ 
+    //Fonction d'ajout d'une nouvelle visite
+    void visite ( int prix, bool proposition, int idbien ){
+    visite.push_back(idbien);
+    visite.push_back( proposition);
+    visite.push_back(prix);
+    };
+ 
+    //fonction pour consulter toutes les visites
+    tab ConsultVisite ()
+    { 
+     return visite;
+    };
+ 
+    // fonction proposition d'une nouvelle visite
+    bool Proposition (int idbien, int prix)
+    { int i;
+     for ( i= 0, i< visite.size(), i++){
+      if (i = idvisite){
+       visite[i] = idvisite;
+       proposition[i] = true;
+       prix[i] = prix;
+      }
+     }
+    };
+ 
+    //destructeur 
     ~Acheteur ();
 };
