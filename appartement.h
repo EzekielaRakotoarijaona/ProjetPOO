81df2011c806cc8355751d18d1bd0b0d4e530773
Maxime.Monge <maxime.monge@etu.u-bordeaux.fr>
2018-04-10 14:30:03 +0200

    ajout main + modif appart.h

--- appartement.h
+++ appartement.h
@@ -9,5 +9,12 @@ protected:
   bool balcon;
   int nombre_appartement_immeuble;
 public:
-
+  inline Appartement() {nombre_de_piece_appartement = 0; etage = 0; garage = false; cave = false; balcon = false; nombre_appartement_immeuble = 0;};
+  inline Appartement(int _nb_pieces, int _etage, bool _garage, bool _cave, bool _balcon, int _nb_appart_imm) {nombre_de_piece_appartement = _nb_pieces; etage = _etage; garage = _garage; cave = _cave; balcon = _balcon; nombre_appartement_immeuble = _nb_appart_imm;};
+  inline int retourner_nombrePieceAppartement() {return nombre_de_piece_appartement;};
+  inline int retourner_etage() {return etage;};
+  inline bool retourner_garage() {return garage;};
+  inline bool retourner_cave() {return cave;};
+  inline bool retourner_balcon() {return balcon;};
+  inline int retourner nbAppartImm() {return nombre_appartement_immeuble;};
 };
