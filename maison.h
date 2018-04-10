17317db8c16a3d71cbaba4f419a8506854ed059d
ezekielarakotoarijaona <ezekrako@gmail.com>
2018-04-10 18:29:39 +0200

    Correction constructeur maison.h

--- maison.h
+++ maison.h
@@ -10,8 +10,7 @@ protected:
   bool jardin;
   bool piscine;
 public:
-  inline maison(){nombre_pieces = 0; garage = false; jardin = false ; piscine = false;};
-  inline maison(int _nombre_pieces, bool _garage, bool _jardin, bool _piscine){nombre_pieces = _nombre_pieces; garage = _garage; jardin = _jardin; piscine = _piscine;};
+  inline maison(int prix, std::string adresse, int surface, std::string ref_client, int _id_bien,int _nombre_pieces, bool _garage, bool _jardin, bool _piscine):Biens(prix,adresse,surface,ref_client,_id_bien){nombre_pieces = _nombre_pieces; garage = _garage; jardin = _jardin; piscine = _piscine;};
   inline retourner_nombre_pieces(){return nombre_pieces;};
   inline retourner_jardin(){return jardin;};
   inline retourner_garage(){return  garage;};
