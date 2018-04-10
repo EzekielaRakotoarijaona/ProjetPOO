8fa2bd782dfe4a764832c386e21eaae9208cb386
ezekielarakotoarijaona <ezekrako@gmail.com>
2018-04-10 23:23:30 +0200

    Debugage massif version 2 en puissance

--- locaux_pro.h
+++ locaux_pro.h
@@ -9,7 +9,7 @@ protected:
   int taille_vitrine;
   bool piece_stockage;
 public:
-  Locaux_pro(int _prix, std::string _adresse, int _surface, int _reference, int _id_bien,int taille, bool stockage);
+  Locaux_pro(int _prix, std::string _adresse, int _surface, std::string _reference, int _id_bien,int taille, bool stockage);
   inline int retourner_taille() {return taille_vitrine;};
   inline bool retourner_stockage() {return piece_stockage;};
   void afficher();
