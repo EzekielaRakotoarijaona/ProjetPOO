5868c17b67b295f1a43f3da2baed988eeef62e6e
lilouNait <38019413+lilouNait@users.noreply.github.com>
2018-04-11 16:03:52 +0200

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -2,18 +2,21 @@
 #include "client.h"
 using namespace std;
 
+//Constructeur sans spécifications 
 Client::Client(){
   nom = "\0";
   adresse = "\0";
   cout << "client cree" << endl;
 }
 
+// constructeur avec spécifications
 Client::Client(string n, string ad){
   nom = n;
   adresse = ad;
   cout << "client cree" << endl;
 }
 
+//Methode d'affichage
 void Client::afficher(){
   cout << "nom client : " << nom << endl << "adresse client : " << adresse << endl;
 }
