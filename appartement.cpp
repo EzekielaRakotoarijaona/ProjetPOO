1afc5184768793e3ab20a0e8f89bfaee26c2e9d5
lilouNait <38019413+lilouNait@users.noreply.github.com>
2018-04-11 15:58:03 +0200

    Update appartement.cpp

--- appartement.cpp
+++ appartement.cpp
@@ -2,7 +2,7 @@
 #include "appartement.h"
 using namespace std;
 
-
+// constructeur 
 Appartement::Appartement(int _prix, std::string _adresse, int _surface, string _reference, int _id_bien,int _nb_pieces, int _etage, bool _garage, bool _cave, bool _balcon, int _nb_appart_imm):Biens(_prix,_adresse,_surface,_reference,_id_bien){
   nombre_de_piece_appartement = _nb_pieces;
   etage = _etage;
@@ -14,11 +14,12 @@ Appartement::Appartement(int _prix, std::string _adresse, int _surface, string _
   this->Biens::afficher();
 }
 
+// Methode qui retourne le nombre de pièce de l'appartement
 int Appartement::retourner_nombrePieceAppartement(){
   return nombre_de_piece_appartement;
 }
 
-
+// Methode d'affichage des informations relatives à l'appartement
 void Appartement::afficher(){
   cout << "nombre de piece appartement : " << nombre_de_piece_appartement << endl;
   cout << "etage : " << etage << endl;
