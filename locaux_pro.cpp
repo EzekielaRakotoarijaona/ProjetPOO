0bc21a567560e977e61e5aa3660c2a0eac106ab4
lilouNait <38019413+lilouNait@users.noreply.github.com>
2018-04-11 16:05:24 +0200

    Update locaux_pro.cpp

--- locaux_pro.cpp
+++ locaux_pro.cpp
@@ -2,14 +2,14 @@
 #include "locaux_pro.h"
 using namespace std;
 
-
+//Fonction constructeur avec spécifications
 Locaux_pro::Locaux_pro(int _prix, std::string _adresse, int _surface, string _reference, int _id_bien,int taille, bool stockage):Biens(_prix,_adresse,_surface,_reference,_id_bien){
   taille_vitrine = taille;
   piece_stockage = stockage;
   cout << "locaux crees" << endl;
   this->Biens::afficher();
 }
-
+//Fonction d'affichage
 void Locaux_pro::afficher(){
   cout << "taille vitrine : " << taille_vitrine << endl << "piece_stockage : " << piece_stockage << endl;
 }
